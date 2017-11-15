2b36c505c9e511aed7372e9084375bddb8756e05
Ihsan Abulhairov <ihsan0302@gmail.com>
2017-11-15 15:36:08 -0800

    вспомогательные методы и класс для Контактов

---
+++ contact.py
@@ -0,0 +1,11 @@
+class Contact:
+    def __init__(self, firstname, middlename, lastname, nickname, company, title, adress, mobile, email):
+        self.firstname = firstname
+        self.middlename = middlename
+        self.lastname = lastname
+        self.nickname = nickname
+        self.company = company
+        self.title = title
+        self.adress = adress
+        self.mobile = mobile
+        self.email = email
