602c1e5c0c0771935ca045a793662ba7bbb9c25f
Ihsan Abulhairov <ihsan0302@gmail.com>
2017-11-17 14:02:56 -0800

    создан class SessionHelper

--- test/test_add_second_group.py
+++ test/test_add_second_group.py
@@ -12,11 +12,11 @@ def app(request):
     return fixture
 
 def test_add_second_group(app):
-    app.login(username="admin", password="secret")
+    app.session.login(username="admin", password="secret")
     app.greate_group(Group(name="second ", header="qwe", footer="asd"))
-    app.logout()
+    app.session.logout()
 
 def test_add_empty_group(app):
-    app.login(username="admin", password="secret")
+    app.session.login(username="admin", password="secret")
     app.greate_group(Group(name="", header="", footer=""))
-    app.logout()
+    app.session.logout()
