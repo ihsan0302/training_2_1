8ee0d9cf84ad65d1408c6f08dc9f21c465299717
Ihsan Abulhairov <ihsan0302@gmail.com>
2017-11-17 23:07:12 -0800

    new test deletion contact

--- fixture/contact.py
+++ fixture/contact.py
@@ -38,6 +38,15 @@ class ContactHelper:
         # submit contact creation
         wd.find_element_by_xpath("//div[@id='content']/form/input[21]").click()
 
+    def delete_first_contact(self):
+        wd = self.app.wd
+        # select first contact
+        wd.find_element_by_name("selected[]").click()
+        # submit deletion
+        wd.find_element_by_xpath("//div[@id='content']/form[2]/div[2]/input").click()
+        wd.switch_to_alert().accept()
+        wd.find_element_by_link_text("home").click()
+
     def return_to_homepage(self):
         wd = self.app.wd
         wd.find_element_by_link_text("home page").click()
