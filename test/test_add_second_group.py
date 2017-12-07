af8e1632b361d274259c6e241fe515b1fc5b7151
Ihsan Abulhairov <ihsan0302@gmail.com>
2017-12-06 19:52:42 -0800

    Cравнения списков для групп

--- test/test_add_second_group.py
+++ test/test_add_second_group.py
@@ -1,9 +1,20 @@
 # -*- coding: utf-8 -*-
 from model.group import Group
 
-
 def test_add_second_group(app):
-    app.group.create(Group(name="second ", header="qwe", footer="asd"))
+    old_groups = app.group.get_group_list()
+    group = Group(name="second", header="qwe", footer="asd")
+    app.group.create(group)
+    new_groups = app.group.get_group_list()
+    assert len(old_groups) + 1 == len(new_groups)
+    old_groups.append(group)
+    assert sorted(old_groups, key=Group.id_or_max) == sorted(new_groups, key=Group.id_or_max)
 
 def test_add_empty_group(app):
-    app.group.create(Group(name="", header="", footer=""))
+    old_groups = app.group.get_group_list()
+    group = Group (name="", header="", footer="")
+    app.group.create(group)
+    new_groups = app.group.get_group_list()
+    assert len(old_groups) + 1 == len(new_groups)
+    old_groups.append(group)
+    assert sorted(old_groups, key=Group.id_or_max) == sorted(new_groups, key=Group.id_or_max)
--- test/test_mod_group.py
+++ test/test_mod_group.py
@@ -3,10 +3,23 @@ from model.group import Group
 def test_modify_first_group_name(app):
     if app.group.count() == 0:
         app.group.create(Group(name=" "))
-    app.group.modify_first_group(Group(name="BBB"))
+    old_groups = app.group.get_group_list()
+    group = Group(name="BBB")
+    group.id = old_groups [0].id
+    app.group.modify_first_group(group)
+    new_groups = app.group.get_group_list()
+    assert len(old_groups) == len(new_groups)
+    old_groups [0] = group
+    assert sorted(old_groups, key=Group.id_or_max) == sorted(new_groups, key=Group.id_or_max)
 
-def test_modify_first_group_header(app):
-    app.group.modify_first_group(Group(header="CCC"))
-
-def test_modify_first_group_footer(app):
-    app.group.modify_first_group(Group(footer="DDD"))
+# def test_modify_first_group_header(app):
+#     old_groups = app.group.get_group_list()
+#     app.group.modify_first_group(Group(header="CCC"))
+#     new_groups = app.group.get_group_list()
+#     assert len(old_groups) == len(new_groups)
+#
+# def test_modify_first_group_footer(app):
+#     old_groups = app.group.get_group_list()
+#     app.group.modify_first_group(Group(footer="DDD"))
+#     new_groups = app.group.get_group_list()
+#     assert len(old_groups) == len(new_groups)
