b19f50df64832d743f6fc151183326741899fe06
Ihsan Abulhairov <ihsan0302@gmail.com>
2017-12-10 15:03:50 -0800

    Поверка для поля телефонов контакта на главной странице

--- test/test_add_contact.py
+++ test/test_add_contact.py
@@ -4,8 +4,9 @@ from model.contact import Contact
 
 def test_add_new_contact(app):
     old_contacts = app.contact.get_contact_list()
-    contact = Contact(firstname="Ivan", middlename="Alex", lastname="Samaryn", nickname="Vano",
-                               company="ZPI", title="Sir", adress="Minsk", mobile="123456789", email="test@test.test")
+    contact = Contact(firstname="Ivan", middlename="Alex", lastname="Samaryn", nickname="Vano", company="ZPI",
+                      title="Sir", address="Minsk", email="test@test.by", email2="test2@test.by", email3="test3@test.by",
+                      homephone="+3333333", mobile="(777)1111", workphone="222 22 22", secondaryphone="444-44-44")
     app.contact.create(contact)
     app.contact.return_to_homepage()
     assert len(old_contacts) + 1 == app.contact.count()
--- test/test_mod_contact_2.py
+++ test/test_mod_contact_2.py
@@ -5,7 +5,7 @@ from random import randrange
 def test_modify_some_contact_2(app):
     if app.contact.count() == 0:
         app.contact.create(Contact(firstname="Jacob", middlename="Kanstantyn", lastname="Kolas", nickname="Lesavik",
-                               company="New Land", title="Sir", adress="Minsk", mobile="+987654321", email="test@kolas.by"))
+                               company="New Land", title="Sir", adress="Minsk", mobilephone="+987654321", email="test@kolas.by"))
         app.contact.return_to_homepage()
     old_contacts = app.contact.get_contact_list()
     index = randrange(len(old_contacts))
