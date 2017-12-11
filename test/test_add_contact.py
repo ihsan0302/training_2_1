c605e5573e2307ca105e54453b1e8760bd6ee1df
Ihsan Abulhairov <ihsan0302@gmail.com>
2017-12-10 18:43:45 -0800

    Параметризация тестов для создания групп и контактов

--- test/test_add_contact.py
+++ test/test_add_contact.py
@@ -1,12 +1,30 @@
 # -*- coding: utf-8 -*-
 from model.contact import Contact
+import pytest
+import string
+import random
 
+def random_string(prefix, maxlen):
+    symbols = string.ascii_letters + string.digits + " "*5
+    return prefix + "".join([random.choice(symbols) for i in range(random.randrange(maxlen))])
 
-def test_add_new_contact(app):
+def random_string_for_phones(prefix, maxlen):
+    symbols = string.digits + " "*2
+    return prefix + "".join([random.choice(symbols) for i in range(random.randrange(maxlen))])
+
+testdata = [Contact(firstname="", middlename="", lastname="", nickname="", company="",title="", address="", email="",
+                    email2="", email3="", homephone="", mobile="", workphone="", secondaryphone="")] + [
+    Contact(firstname=random_string("Fname", 15), middlename=random_string("Mname", 15), lastname=random_string("Lname", 15),
+            nickname=random_string("nick", 10), company=random_string("company", 20),title=random_string("title", 15),
+            address=random_string("address", 30), email=random_string("email", 30), email2=random_string("email2", 30),
+            email3=random_string("email3", 30), homephone=random_string_for_phones("+",15), mobile=random_string_for_phones("+",15),
+            workphone=random_string_for_phones("+",15), secondaryphone=random_string_for_phones("+",15))
+    for i in range(3)
+]
+
+@pytest.mark.parametrize("contact", testdata, ids=[repr(x) for x in testdata])
+def test_add_new_contact(app, contact):
     old_contacts = app.contact.get_contact_list()
-    contact = Contact(firstname="Ivan", middlename="Alex", lastname="Samaryn", nickname="Vano", company="ZPI",
-                      title="Sir", address="Minsk", email="test@test.by", email2="test2@test.by", email3="test3@test.by",
-                      homephone="+3333333", mobile="(777)1111", workphone="222 22 22", secondaryphone="444-44-44")
     app.contact.create(contact)
     app.contact.return_to_homepage()
     assert len(old_contacts) + 1 == app.contact.count()
@@ -14,13 +32,3 @@ def test_add_new_contact(app):
     old_contacts.append(contact)
     assert sorted(old_contacts, key=Contact.id_or_max) == sorted(new_contacts, key=Contact.id_or_max)
 
-# def test_add_second_contact(app):
-#     old_contacts = app.contact.get_contact_list()
-#     contact = Contact(firstname="Jacob", middlename="Kanstantyn", lastname="Kolas", nickname="Lesavik",
-#                                company="New Land", title="Sir", adress="Minsk", mobile="+987654321", email="test@kolas.by")
-#     app.contact.create(contact)
-#     app.contact.return_to_homepage()
-#     assert len(old_contacts) + 1 == app.contact.count()
-#     new_contacts = app.contact.get_contact_list()
-#     old_contacts.append(contact)
-#     assert sorted(old_contacts, key=Contact.id_or_max) == sorted(new_contacts, key=Contact.id_or_max)
